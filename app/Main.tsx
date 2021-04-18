734693b245d22df93aa856870f6243db16c4f61a
Aurélien Bertron <aurelien.bertron@stormshield.eu>
2021-04-18 14:08:58 +0200

    remove bar

--- app/Main.tsx
+++ app/Main.tsx
@@ -2,7 +2,6 @@ import React, { useState } from "react";
 
 import { Task } from "./models.ts";
 
-import Bar from "./Bar.tsx";
 import Editor from "./Editor.tsx";
 import TasksList from "./TasksList.tsx";
 
@@ -40,11 +39,15 @@ export default function Main() {
   return (
     <div>
       <h1>Create Jira tasks</h1>
-      <Editor
-        task={initTask}
-        onAdd={onAdd}
-      />
-      <Bar onCopy={onCopy} />
+      <div>
+        <Editor
+          task={initTask}
+          onAdd={onAdd}
+        />
+        <button onClick={onCopy}>
+          Copy to clipboard
+        </button>
+      </div>
       <TasksList tasks={tasks} />
     </div>
   );
